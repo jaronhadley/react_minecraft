23a89a4e1daebce54d8d3e2c28bb0d61aa7e1926
Jaron Hadley <jaron.hadley@gmail.com>
2022-10-16 13:26:25 -0600

    added ground

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,14 +1,14 @@
 import {dirtImg,logImg,grassImg,glassImg,woodImg} from './images' 
 import {TextureLoader} from 'three';
 
-const dirtTexture = new TextureLoader(dirtImg);
-const logTexture = new TextureLoader(logImg);
-const grassTexture = new TextureLoader(grassImg);
-const glassTexture = new TextureLoader(glassImg);
-const woodTexture = new TextureLoader(woodImg);
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
 
 // separate texture for ground
-const groundTexture = new TextureLoader(grassImg);
+const groundTexture = new TextureLoader().load(grassImg);
 
 export {
     dirtTexture,
