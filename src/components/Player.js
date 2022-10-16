aae182ca640daaafd9306b73ce8e3f73d943823f
Jaron Hadley <jaron.hadley@gmail.com>
2022-10-16 14:32:17 -0600

    player controls added

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,10 +4,17 @@ import { Vector3 } from "three";
 import { useRef, useEffect } from 'react';
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 4;
+const SPEED = 4;
 
 export const Player = () => {
-    const actions = useKeyboard()
-    console.log('actions', Object.entries(actions).filter(([k,v]) => v))
+    const {moveBackward,moveForward,moveLeft,moveRight,jump} = useKeyboard()
+    //console.log(moveBackward)
+    //console.log(moveForward)
+    //console.log(moveLeft)
+    //console.log(moveRight)
+    //console.log(jump)
+
     const {camera} = useThree();
     const [ref, api] = useSphere(() => ({
         mass :1,
@@ -29,6 +36,32 @@ export const Player = () => {
         camera.position.copy(new Vector3(pos.current[0],pos.current[1],pos.current[2]))
 
         //api.velocity.set(0,1,0)
+        const direction = new Vector3()
+
+        // forward back
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 : 0),
+        )
+        // left right
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0,
+        )
+        
+        direction.subVectors(frontVector,sideVector)
+        .normalize()
+        .multiplyScalar(SPEED)
+        .applyEuler(camera.rotation)
+
+        api.velocity.set(direction.x, vel.current[1], direction.z)
+
+        // jumping
+        if(jump && Math.abs(vel.current[1])<0.05) {
+            api.velocity.set(vel.current[0],JUMP_FORCE,vel.current[2])
+        }
     })
 
     return (
